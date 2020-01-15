cc88154aae15e3e05b2eeedf7735b9bb2ccfede8
Yuet Loo Wong <contact@yuetloo.com>
2020-01-15 16:38:08 -0500

    change argument name

--- IERC20.sol
+++ IERC20.sol
@@ -11,12 +11,12 @@ interface IERC20 {
 
     // mandatory
     function totalSupply() external view returns (uint256);
-    function balanceOf(address _owner) external view returns (uint256 balance);
-    function transfer(address _to, uint256 _value) external returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
-    function approve(address _spender, uint256 _value) external returns (bool success);
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function transfer(address recipient, uint256 amount) external returns (bool success);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool success);
+    function approve(address spender, uint256 amount) external returns (bool success);
+    function allowance(address owner, address spender) external view returns (uint256 remaining);
+    event Transfer(address indexed sender, address indexed recipient, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 }
 
