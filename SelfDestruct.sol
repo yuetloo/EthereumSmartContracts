27be286efba3528a28944b833afb5a07fe69a6d7
Yuet Loo Wong <contact@yuetloo.com>
2020-01-20 18:10:44 -0500

    add more function to demo the feature of selfdestruct

--- SelfDestruct.sol
+++ SelfDestruct.sol
@@ -1,24 +1,33 @@
 pragma solidity 0.5.16;
 
 contract Bank {
+    constructor() public payable {}
+    function getInterest() public pure returns(uint) {return 6;}
     function getBalance() public view returns (uint) {return address(this).balance;}
-    function () external payable { revert(); }
+    function () external payable { revert("bank does not accept ether"); }
     function kill(address payable target) public {
         selfdestruct(target);
     }
 }
 contract Caller {
-    constructor() public payable {}
-    function doTransfer(address payable user) public { 
-        user.transfer(1 ether); 
+    Bank public bank;
+
+    constructor(Bank theBank) public payable {
+        bank = theBank;
     }
-    function doSend(address payable user) public { 
+    function doTransfer(address payable user) public {
+        user.transfer(1 ether);
+    }
+    function doSend(address payable user) public {
         require(user.send(1 ether));
     }
     function kill(address payable target) public {
         selfdestruct(target);
     }
-    function getBalance() public view returns (uint) { 
-        return address(this).balance; 
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    function getInterest() public view returns(uint) {
+        return bank.getInterest();
     }
 }
